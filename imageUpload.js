4ab20e4c45b6c616ea7d524870fea1ee43552560
hakuyoka <hakuyoka@gmail.com>
2017-07-06 15:40:35 +0900

    commit

--- imageUpload.js
+++ imageUpload.js
@@ -4,7 +4,7 @@
 var fs = require('fs');
 var WebClient = require('@slack/client').WebClient;
 
-var token = process.env.SLACK_API_TOKEN || 'xoxp-154548540966-154559871527-209344800103-94e43eb444a1433f328cab68cc985864'; //see section above on sensitive data
+var token = process.env.SLACK_API_TOKEN || ''; //see section above on sensitive data
 
 var web = new WebClient(token);
 
